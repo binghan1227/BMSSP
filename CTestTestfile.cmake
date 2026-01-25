a76a4c43619902bf9b2e7505e68cd25b6db0d28b
binghan1227 <binghan1227@126.com>
2026-01-25 21:36:41 +0800

    remove ctest.cmake file

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/Coding/bmssp
-# Build directory: /home/pi/Coding/bmssp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BlockListTest "/home/pi/Coding/bmssp/test_block_list")
-set_tests_properties(BlockListTest PROPERTIES  _BACKTRACE_TRIPLES "/home/pi/Coding/bmssp/CMakeLists.txt;22;add_test;/home/pi/Coding/bmssp/CMakeLists.txt;0;")
